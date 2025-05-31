a7efe299f4f692d714e6919f6da2972539a64d09
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-01 01:55:22 +0530

    modified about

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,17 @@
+<script setup>
+useHead({
+  title: 'About | My Portfolio'
+})
+
+const randomId = Math.floor(Math.random() * 200) + 1
+
+// Destructuring only data
+const { data: todo } = await useAsyncData('todo', () =>
+  $fetch('https://jsonplaceholder.typicode.com/todos/' + randomId),
+  { server: false }
+)
+</script>
+
 <template>
   <section>
     <h1>About Me</h1>
@@ -6,28 +20,15 @@
       This portfolio is built with Nuxt.js!
     </p>
 
-    <div v-if="todo.data.value">
+    <div v-if="todo">
       <h2>Random Todo</h2>
-      <p><strong>ID:</strong> {{ todo.data.value.id }}</p>
-      <p><strong>Title:</strong> {{ todo.data.value.title }}</p>
-      <p><strong>Completed:</strong> {{ todo.data.value.completed ? 'Yes' : 'No' }}</p>
+      <p><strong>ID:</strong> {{ todo.id }}</p>
+      <p><strong>Title:</strong> {{ todo.title }}</p>
+      <p><strong>Completed:</strong> {{ todo.completed ? 'Yes' : 'No' }}</p>
     </div>
 
-    <div v-else-if="todo.pending">
-      <p>Loading todo...</p>
-    </div>
-
-    <div v-else-if="todo.error">
-      <p>Error loading todo.</p>
+    <div v-else>
+      <p>Loading or error...</p>
     </div>
   </section>
 </template>
-
-<script setup>
-useHead({
-  title: 'About | My Portfolio'
-})
-
-const randomId = Math.floor(Math.random() * 200) + 1
-const todo = await useFetch('https://jsonplaceholder.typicode.com/todos/' + randomId)
-</script>
