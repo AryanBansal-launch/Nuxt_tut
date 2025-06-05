1fc78e576d416b54babeb449e50d50af6755df07
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-05 18:29:46 +0530

    change api in isr

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div>
       <h1>Blog (ISR) - slug: {{ route.params.slug }}</h1>
       <pre>{{ user }}</pre>
@@ -25,4 +25,34 @@
     })
   )
   </script>
-  
+   -->
+
+<template>
+  <div>
+    <h1>Blog (ISR) - slug: {{ route.params.slug }}</h1>
+    <pre>{{ user }}</pre>
+    <p>Fetched at: {{ user?.fetchedAt }}</p>
+    <p><strong>Random Number:</strong> {{ user?.randomNum }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+definePageMeta({
+  isr: {
+    expiration: 60 // Revalidate every 60 seconds
+  }
+})
+
+const route = useRoute()
+
+const { data: user, error } = await useAsyncData(`custom-api-${route.params.slug}`, () =>
+  $fetch('https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello')
+    .then(res => {
+      // Preserve timestamp logic
+      res.fetchedAt = new Date().toISOString()
+      return res
+    })
+)
+</script>
