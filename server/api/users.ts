d61d4eea5576c715faaa5b5f94aa5209771dc682
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-09 17:54:57 +0530

    add no store cache header to user api route

--- server/api/users.ts
+++ server/api/users.ts
@@ -1,5 +1,7 @@
 export default defineEventHandler(async () => {
-  const res = await $fetch('https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello')
+  const res = await $fetch('https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello',{
+    cache: 'no-store' // Disable CDN cache
+  })
   return res
 })
 
