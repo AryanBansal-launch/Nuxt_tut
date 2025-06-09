557805e26e6b0e8ecf3ab356698335f75a743360
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-09 18:06:19 +0530

    Modified users api route

--- server/api/users.ts
+++ server/api/users.ts
@@ -1,8 +1,15 @@
-export default defineEventHandler(async () => {
-  const res = await $fetch('https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello',{
-    cache: 'no-store' // Disable CDN cache
-  })
+// export default defineEventHandler(async () => {
+//   const res = await $fetch('https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello',{
+//     cache: 'no-store'
+//   })
+//   return res
+// })
+
+// server/api/hello.nocache.ts
+export default defineEventHandler(async (event) => {
+  setHeader(event, 'Cache-Control', 'no-store')
+  const res = await $fetch(
+    'https://nextjs-ssr-isr-demo-wce75vr67bt8.devcontentstackapps.com/api/hello'
+  )
   return res
 })
-
-  
