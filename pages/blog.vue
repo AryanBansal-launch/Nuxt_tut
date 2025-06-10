a5bc622e120d66f0418ab44851d9c0f901c28c3b
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-10 16:16:13 +0530

    added language to static isr blog route

--- pages/blog.vue
+++ pages/blog.vue
@@ -5,9 +5,14 @@
     <p>Fetched at: {{ user?.fetchedAt }}</p>
     <p><strong>Random Number:</strong> {{ user?.randomNum }}</p>
   </div>
+  <button v-for="locale in locales" @click="setLocale(locale.code)">
+      {{ locale.name }}
+    </button>
+    <h1>{{ $t('welcome') }}</h1>
 </template>
 
 <script setup>
+const { locales, setLocale } = useI18n()
 definePageMeta({
   isr: {
     expiration: 60 // Revalidate every 60 seconds
