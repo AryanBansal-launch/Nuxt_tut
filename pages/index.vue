fe243828c9a53ce2e7ae84ec75e42090a0343c8b
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-10 14:36:32 +0530

    removed localization from home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,12 @@
       <p class="hero-subtitle">A passionate web developer crafting modern web experiences.</p>
       <NuxtLink to="/projects" class="hero-button">View My Work</NuxtLink>
     </section>
-     <button v-for="locale in locales" @click="setLocale(locale.code)">
-      {{ locale.name }}
-    </button>
-    <h1>{{ $t('welcome') }}</h1>
   </template>
   
   <script setup>
   useHead({
     title: 'Home | Aryan Bansal Portfolio'
   })
-  const { locales, setLocale } = useI18n()
   </script>
   
   <style scoped>
