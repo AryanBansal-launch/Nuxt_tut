13f5e1ca881620943a026fc1e726e91174095f0f
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-16 17:30:26 +0530

    added a redirect from about to projects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "/blog": {
-      isr: 5,
+      isr: 60,
       headers:{
          "Cache-Control":"public, max-age=0, s-maxage=60, stale-while-revalidate=30"
       }
