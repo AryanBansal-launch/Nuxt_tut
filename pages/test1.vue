571efae2b58f88047a5636f59879b67224209823
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-20 18:48:22 +0530

    added a middleware to reirect from test1 to test2

--- pages/test1.vue
+++ pages/test1.vue
@@ -3,4 +3,11 @@
     <h1>This is test1 page</h1>
   </div>
 </template>
+// in pages/test1.vue
+<script setup>
+definePageMeta({
+  middleware: () => navigateTo('/test2', { redirectCode: 301 })
+})
+</script>
+
 
