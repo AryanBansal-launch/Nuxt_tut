5f3ac4fd664268860da2299eb297009c921b2374
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-23 11:30:58 +0530

    reoved redirect from launch json and added to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,8 +46,9 @@ export default defineNuxtConfig({
         "Cache-Control":"public, max-age=0, s-maxage=60, stale-while-revalidate=30"
       } 
     },
-    "/isr-demo": { isr: 30 },  // Rebuilds once every 10 seconds
-    "/swr-demo": { swr: 30 }, // Revalidates every 10 seconds on every request
+    "/isr-demo": { isr: 30 },  
+    "/swr-demo": { swr: 30 }, 
+    "/test1" :{redirect: "/test2"}
   },
   experimental: {
     defaults: {
