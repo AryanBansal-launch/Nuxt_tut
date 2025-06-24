d76c51982085dc6d1f5d3724d19c93b81dfbfeb8
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-24 13:06:01 +0530

    Added redirects from all 2 places

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -10,7 +10,6 @@
           <NuxtLink to="/blog" exact-active-class="active" :prefetch="false">Blog_static_ISR</NuxtLink>
           <NuxtLink to="/test" exact-active-class="active" :prefetch="false">Test_Dynamic_ISR</NuxtLink>
           <NuxtLink to="/test1" exact-active-class="active" :prefetch="false">Test1</NuxtLink>
-           <!-- <a href="/test1">Test1</a> -->
           <NuxtLink to="/test2" exact-active-class="active" :prefetch="false">Test2</NuxtLink>
           <a href="/test3">Test3</a>
           <a href="/test4">Test4</a>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,8 +48,8 @@ export default defineNuxtConfig({
     },
     "/isr-demo": { isr: 30 },  
     "/swr-demo": { swr: 30 }, 
-    // "/test1" :{redirect: "/test2"}
-    "/test1":{proxy: "https://nuxttut-test.devcontentstackapps.com/test2"}
+    "/test1" :{redirect: "/test2"}
+    // "/test1":{proxy: "https://nuxttut-test.devcontentstackapps.com/test2"}
   },
   experimental: {
     defaults: {
