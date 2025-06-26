13cfbd5f3369ec6421ba64b921d62afac7dfc49f
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-26 18:23:49 +0530

    server routes vs api

--- pages/test4.vue
+++ pages/test4.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
       <h1>This is test4 page</h1>
-      <p>Hello: {{ val }}</p>
+      <p>Data from /server/routes/hello: {{ val }}</p>
+      <p>Data from /server/api/data: {{ val2 }}</p>
     </div>
   </template>
    <script setup>
@@ -12,6 +13,8 @@
        'Expires': '0'
      }
    })
+   const { data: data2 } = await useFetch('/api/data')
    const val = data.value.timestamp;
+   const val2 = data2.value.timestamp;
    </script>
   
