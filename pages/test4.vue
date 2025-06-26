a7b55e86f18a76a548fe50e6c1e4b75a47d383d7
Aryan Bansal <aryan.bansal@contentstack.com>
2025-06-26 16:05:18 +0530

    added server route

--- pages/test4.vue
+++ pages/test4.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
       <h1>This is test4 page</h1>
+      <p>Hello: {{ val }}</p>
     </div>
   </template>
-  
+   <script setup>
+   const { data } = await useFetch('/hello')
+   const val = data.value;
+   </script>
   
