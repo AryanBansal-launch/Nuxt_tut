d7ecb11e34f3e72a87c1f36d35d4049fd55388a8
Aryan Bansal <aryan.bansal@contentstack.com>
2025-07-02 12:01:10 +0530

    update log in api

--- server/api/data.ts
+++ server/api/data.ts
@@ -1,5 +1,7 @@
 export default defineEventHandler(() => {
   console.log('data API called')
+  const sample="Aryan";
+  console.log("Log from data API",sample);
   return {
     timestamp: new Date().toISOString(),
   }
